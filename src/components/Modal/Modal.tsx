c770d5da82fc834b34948bd9e3660a3bd60a2a6c
Evgeniy <charizardik64@gmail.com>
2025-05-28 13:04:08 +0300

    feat: Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,23 @@
-export default function Modal() {
+import styles from "./Modal.module.css"
+import {XIcon} from "lucide-react";
+import Button from "../Button/Button.tsx";
+
+
+export default function Modal({title, children, onClose}: {
+  title: string,
+  onClose: () => void,
+  children: any
+}) {
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className={styles.overlay}>
+      <div className={styles.window}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>{title}</h1>
+          <Button onClick={onClose} icon={<XIcon size={18}/>}/>
+        </div>
+
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
