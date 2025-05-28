db92f257a7e500746746ad2808dada1681deffc1
Evgeniy <charizardik64@gmail.com>
2025-05-28 13:03:35 +0300

    feat: SectionTitle

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,7 +1,7 @@
-export default function SectionTitle() {
+import style from "./SectionTitle.module.css";
+
+export default function SectionTitle({ children }: {children: string}) {
   return (
-    <>
-    
-    </>
-  )
+      <h1 className={style.sectionTitle}>{children}</h1>
+  );
 }
