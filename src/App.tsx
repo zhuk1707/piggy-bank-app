517b00863896550747ef0d1c879409fd4933497e
Evgeniy <charizardik64@gmail.com>
2025-05-28 13:11:15 +0300

    feat: App changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import './App.css'
+import Dashboard from './components/Dashboard/Dashboard.tsx';
+import Footer from './components/Footer/Footer.tsx';
 import Header from "./components/Header/Header.tsx";
 import CardsContainer from "./components/СardContainer/CardsContainer.tsx";
+import {useState} from "react";
+import Modal from "./components/Modal/Modal.tsx";
 
-function App() {
+export default function App() {
+  const [isModalOpen, setIsModalOpen] = useState(true)
 
   return (
     <>
       <Header/>
 
+      <div className="container">
+
+        <button
+          style={{width: "100%"}}
+          onClick={() => setIsModalOpen(true)}
+        >Open</button>
+      </div>
+
+      {isModalOpen &&
+        <Modal
+          title={"Creating a goal"}
+          onClose={() => setIsModalOpen(false)}
+        >
+          <p>some</p>
+        </Modal>}
+
       <main className="main">
         <div className="container">
-
           <CardsContainer/>
-
+          <Dashboard/>
         </div>
-
-
       </main>
+
+      <Footer/>
     </>
-  )
+  );
 }
 
-export default App
--- src/components/СardContainer/CardsContainer.tsx
+++ src/components/СardContainer/CardsContainer.tsx
@@ -1,16 +1,20 @@
-import Card from "../Card/Card.tsx";
+import Card, {type CardProps} from "../Card/Card.tsx";
+import SectionTitle from "../SectionTitle/SectionTitle.tsx";
+
+const cardsData: CardProps[] = [
+  {title: "Trip", subtitle: "Travel opens up new horizons :)", deposit: 100, goal: 2500},
+  {title: "PC", deposit: 0, goal: 3000},
+  {title: "Vans", subtitle: "New shoes", deposit: 5, goal: 70},
+  {title: "Car", deposit: 8000, goal: 16000},
+];
 
 export default function CardsContainer() {
-  const cardsData = [
-    {title: "Dream Trip", subtitle: "Travel opens up new horizons", deposit: 100, goal: 2500},
-    {title: "Dream Trip", subtitle: "Travel opens up new horizons", deposit: 1250, goal: 2500},
-  ];
-
-  return (
+  return (<>
+    <SectionTitle>Goals</SectionTitle>
     <div className="cards">
-      {cardsData.map((card, index) => (
-        <Card key={index} {...card} />
+      {cardsData.map((card) => (
+        <Card key={card.title} {...card} />
       ))}
     </div>
-  );
+  </>)
 }
